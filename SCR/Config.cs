96a4531426df61ca1ff3cb8ff04fb7e61c091357
Swishhyy <dylan.minatrea@gmail.com>
2025-04-12 18:38:58 -0500

    Project Init

---
+++ SCR/Config.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Interfaces;
+
+using System.ComponentModel;
+
+namespace SCR
+{
+    public class Config : IConfig
+    {
+        [Description("Enable or disable the plugin.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Enable or disable debug.")]
+        public bool Debug { get; set; } = false;
+    }
+}
