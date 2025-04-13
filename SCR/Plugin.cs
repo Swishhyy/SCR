1b54cfee69bfb760a70d0bd611bcee8f6f5ef5d5
Swishhyy <dylan.minatrea@gmail.com>
2025-04-12 19:09:04 -0500

    Project Init AGAIN

---
+++ SCR/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCR
+{
+    public class Plugin : Plugin<Config>
+    {
+        // Override the plugin's name property.
+        public override string Name => "SCR";
+        // Override the plugin's author property.
+        public override string Author => "Swishhyy";
+        // Override the plugin's version property.
+        public override Version Version => new(1, 0, 0);
+    }
+}
